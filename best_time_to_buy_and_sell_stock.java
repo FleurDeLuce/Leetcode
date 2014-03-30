3f3692827ca585cacdb87c0aa013c5444a17062c
FleurDeLuce <jieqi@reeliolabs.com>
2014-03-30 06:31:35 -0400

    modified profit and min

--- best_time_to_buy_and_sell_stock.java
+++ best_time_to_buy_and_sell_stock.java
@@ -3,10 +3,24 @@ Say you have an array for which the ith element is the price of a given stock on
 
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 */
+// solution 1
 public class Solution {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
         int min = Integer.MAX_VALUE, profit = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) min = prices[i];
+            if (prices[i] - min > profit) profit = prices[i] - min;
+        }
+        return profit;
+    }
+}
+
+// solution 2
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int min = prices[0], profit = 0;
         for (int i = 0; i < prices.length; i++) {
             if (prices[i] < min) min = prices[i];
             if (prices[i] - min > profit) profit = prices[i] - min;
