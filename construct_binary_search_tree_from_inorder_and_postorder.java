52d63e56831c150d4a5c78ff73c65e3ea1753b6e
FleurDeLuce <jieqi@reeliolabs.com>
2014-03-30 04:34:42 -0400

    construct BST from inorder and postorder traversal

--- construct_binary_search_tree_from_inorder_and_postorder.java
+++ construct_binary_search_tree_from_inorder_and_postorder.java
@@ -13,17 +13,28 @@ You may assume that duplicates do not exist in the tree.
  *     TreeNode(int x) { val = x; }
  * }
  */
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if (preorder == null || inorder == null || preorder.length == 0 || inorder.length == 0) return null;
+        if (postorder == null || inorder == null || postorder.length == 0 || inorder.length == 0) return null;
         return buildTreeBottomUp(postorder, inorder, postorder.length - 1, 0, inorder.length - 1);
     }
-    public TreeNode buildTreeBottomUp(int[] inorder, int[] postorder, int postRoot, int inStart, int inEnd) {
+    public TreeNode buildTreeBottomUp(int[] postorder, int[] inorder, int postRoot, int inStart, int inEnd) {
         if (inStart > inEnd) return null;
-        TreeNode root = new TreeNode(preorder[postRoot]);
-        int inRoot = getRootIndex(preorder[postRoots], inorder, inStart, inEnd);
-        int leftLen = inRoot - inStart;
-        
+        TreeNode root = new TreeNode(postorder[postRoot]);
+        int inRoot = getRootIndex(postorder[postRoot], inorder, inStart, inEnd);
+        int rightLen = inEnd - inRoot;
+        if (inRoot > inStart) root.left = buildTreeBottomUp(postorder, inorder, postRoot - rightLen - 1, inStart, inRoot - 1);
+        if (inRoot < inEnd) root.right = buildTreeBottomUp(postorder, inorder, postRoot - 1, inRoot + 1, inEnd);
+        return root;
     }
     
     public int getRootIndex(int val, int[] inorder, int start, int end) { 
