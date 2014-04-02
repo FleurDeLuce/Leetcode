ddbb8c622a56c2bdb3413d9974cfaec4e0275b74
FleurDeLuce <jieqi@reeliolabs.com>
2014-04-02 07:22:02 -0400

    notes to the unique BST solution added

--- unique_binary_search_tree.java
+++ unique_binary_search_tree.java
@@ -1,4 +1,6 @@
 // Time Complexity: O(n^2), Space Complexity: O(n)
+// Good explanation: https://www.youtube.com/watch?v=UfA_v0VmiDg
+// http://fisherlei.blogspot.com/2013/03/leetcode-unique-binary-search-trees.html
 // Solution 1
 public class Solution {
     public int numTrees(int n) {
@@ -6,6 +8,11 @@ public class Solution {
         int tree[] = new int[n + 1];
         tree[0] = 1;
         tree[1] = 1;
+        // the selection of root each time decides the number of unique BST's
+        // in each case, the number of BST is equal to 
+        // the number of BST elements smaller than the root construct times
+        // the number of BST elements larger than the root construct
+        // summing all cases gives the total tree number
         for (int i = 2; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 tree[i] += tree[j] * tree[i - 1 - j];
