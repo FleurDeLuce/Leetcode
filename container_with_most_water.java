5aabce53baef1384400decce26cfc0a5242efb1a
FleurDeLuce <jillchih.cs@gmail.com>
2014-06-30 19:24:02 -0400

    analysis and notes added

--- container_with_most_water.java
+++ container_with_most_water.java
@@ -5,8 +5,9 @@ Note: You may not slant the container.
 */
 
 /*
-Time complexity: O(n)
-Space complexity: O(1)
+Solution: two pointer
+Time complexity: O(n), Space complexity: O(1)
+Ref: http://www.cnblogs.com/remlostime/archive/2012/10/29/2745018.html
 */
 
 public class Solution {
@@ -21,9 +22,13 @@ public class Solution {
         int volume = 0;
         int maxVolume = 0;
 
+        // starting from the widest range of all vertical bars, then shrink the width of container considering the shorter bar height
         while (left < right) {
             volume = Math.min(height[left], height[right]) * (right - left);
             maxVolume = Math.max(maxVolume, volume);
+            // left and right pointers only move when we find a bar taller than both of the two
+            // Greedy algorithm here
+            // now left bar is the bottleneck, if moves the right one left to shrink, it does nothing but reduce the size
             if (height[left] <= height[right]) {
                 left++;
             }
