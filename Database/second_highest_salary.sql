12dcbc16bfd9eb9b4f8e30161507e0ad8e2fe728
FleurDeLuce <jillchih.data@gmail.com>
2015-01-13 23:27:00 -0800

    two alternative solutions added

--- Database/second_highest_salary.sql
+++ Database/second_highest_salary.sql
@@ -11,6 +11,19 @@ Write a SQL query to get the second highest salary from the Employee table.
 For example, given the above Employee table, the second highest salary is 200. If there is no second highest salary, then the query should return null.
  */
 
+# solution 1
 select max(salary)
 from Employee
 where salary < (select max(salary) from Employee);
+
+
+# solution 2
+select max(salary)
+from Employee
+where salary not in (select max(salary) from Employee);
+
+
+# solution 3
+select max(salary)
+from Employee
+where salary <> (select max(salary) from Employee);
