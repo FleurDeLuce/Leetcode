3f4882d21b5ea40ba9da2a4fbe239a4405cbd9f6
FleurDeLuce <jillchih.data@gmail.com>
2015-01-20 01:03:10 -0800

    user-defined solution 2 added

--- Database/consecutive_numbers.sql
+++ Database/consecutive_numbers.sql
@@ -27,10 +27,21 @@ from Logs l1, Logs l2, Logs l3
 where l1.Id + 1 = l2.Id and l1.Num = l2.Num
 and l2.Id + 1 = l3.Id and l2.Num = l3.Num
 
-# user-defined solution
+# user-defined solution 1
 # reference: http://www.madcoder.cn/leetcode-consecutive-numbers.html
 SELECT DISTINCT Num FROM (
     SELECT Num, IF(Num = @last, @row := @row + 1, @row := 1) as times, @last := Num
     FROM Logs, (SELECT @last := 'x', @row := 0) r
 ) t
 WHERE times >= 3;
+
+# user-defined solution 2
+# reference: http://bookshadow.com/weblog/2015/01/14/leetcode-consecutive-numbers/
+SELECT DISTINCT Num FROM (
+  SELECT Num, COUNT(Rank) AS Cnt FROM (
+    SELECT    Num,
+              @curRank := @curRank + IF(@prevNum = Num, 0, 1) AS rank, @prevNum := Num
+    FROM      Logs s, (SELECT @curRank := 0) r, (SELECT @prevNum := NULL) p
+    ORDER BY  ID ASC
+  ) t GROUP BY Rank HAVING Cnt >= 3 
+) n;
